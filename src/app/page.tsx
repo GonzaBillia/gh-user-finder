eb77af1e976b185c3c319e18dabb39480132985a
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-19 18:47:45 -0300

    configuro fuente, bg y centrado de objetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import SearchIcon from '@/components/icons/SearchIcon'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div>
+        <form className='flex items-center gap-2 bg-[#1F2A47] p-2 rounded-xl'>
+          <span>
+            <SearchIcon className='fill-[#0079FF] min-w-5 max-w-7'/>
+          </span>
+          <input type="search" name="search" id="search-input" placeholder='Search GitHub Username' className='h-14 flex-1 bg-transparent p-2 rounded-lg placeholder-white focus:outline-none focus:ring-2 focus:ring-[#0079FF]'/>
+          <button className='bg-[#0079FF] text-white font-bold rounded-lg p-4'>Search</button>
+        </form>
+      </div>
+      <div>
+        <article>
+          Descripcion de pagina
+        </article>
+      </div>
+    </>
   )
 }
 
